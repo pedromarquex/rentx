4fae9590605f197fc5f25e2d569326daa74f6fdd
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-06-10 13:27:42 -0300

    add validation to category creation

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -9,9 +9,23 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
+  const categoryExists = categoriesRepository.findByName(name);
+
+  if (categoryExists) {
+    return response
+      .status(400)
+      .json({ error: 'A Category with this name already exists' });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
