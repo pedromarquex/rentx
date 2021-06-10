304079bd92d21578f3c373790634a0633e0c2917
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-06-10 15:56:05 -0300

    add CreateCategoryService

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,22 @@
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const categoryExists = this.categoriesRepository.findByName(name);
+
+    if (categoryExists) {
+      throw new Error('A Category with this name already exists');
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
