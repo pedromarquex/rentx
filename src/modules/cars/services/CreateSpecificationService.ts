21df93bdc173780e5e37c3d992ca36dfe2233533
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-06-12 10:16:12 -0300

    add specifications routes and services

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,24 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute(): void {
-    console.log('TODO');
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationExists = this.specificationsRepository.findByName(name);
+
+    if (specificationExists) {
+      throw new Error('A Specification with this name already exists');
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
   }
 }
 
