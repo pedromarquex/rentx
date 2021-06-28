431a5a7b4bbed50a6ef339079614536014740f24
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-06-28 00:29:57 -0300

    change specification.routes to use controllers

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,21 +1,11 @@
 import { Router } from 'express';
 
-import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+import { createSpecificationsController } from '../modules/cars/useCases/createSpecification';
 
 const specificationRoutes = Router();
 
-const specificationRepository = new SpecificationsRepository();
-
 specificationRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-  const createSpecificationService = new CreateSpecificationService(
-    specificationRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
+  return createSpecificationsController.handle(request, response);
 });
 
 export { specificationRoutes };
